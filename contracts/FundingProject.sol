da78d7f599f280573f56283dabf408eebfd176e9
Nicolas Posa <nicolasposa@Nicolass-MacBook-Air.local>
2022-06-07 17:40:13 -0300

    events for FundProject and ChangeStatus functions

--- contracts/FundingProject.sol
+++ contracts/FundingProject.sol
@@ -7,41 +7,50 @@ contract FundingProject {
   string public description;
   string public status = "Open";
   address payable public author;
-  uint public fundingGoal;
-  uint public fundingRaised;
+  uint256 public fundingGoal;
+  uint256 public fundingRaised;
 
-  constructor(string memory _id, string memory _name, string memory _description, uint _fundingGoal) {
+  constructor(
+    string memory _id,
+    string memory _name,
+    string memory _description,
+    uint256 _fundingGoal
+  ) {
     id = _id;
     name = _name;
     description = _description;
     fundingGoal = _fundingGoal;
     author = payable(msg.sender);
-  }  
+  }
+
+  event FundProject(
+    address sender,
+    uint256 amount
+  );
+
+  event ChangeStatus(
+    address changer,
+    string newStatus
+  );
 
   modifier isAuthor() {
-    require(
-      author == msg.sender,
-      "Only the author can do this"
-    );
+    require(author == msg.sender, "Only the author can do this");
     _;
   }
 
   modifier isNotAuthor() {
-    require(
-      author != msg.sender, 
-      "The author cannot do this"
-    );
+    require(author != msg.sender, "The author cannot do this");
     _;
   }
 
-  function fundProject() payable public {
+  function fundProject() public payable {
     author.transfer(msg.value);
     fundingRaised += msg.value;
+    emit FundProject(msg.sender, msg.value);
   }
 
   function changeStatus(string calldata newStatus) public {
     status = newStatus;
+    emit ChangeStatus(msg.sender, newStatus);
   }
-
-
-  }
+}
