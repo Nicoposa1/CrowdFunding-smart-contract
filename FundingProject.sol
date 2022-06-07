e3aa74d4fe420e199758e9f5373b018b64082ca3
Nicolas Posa <nicolasposa@Nicolass-MacBook-Air.local>
2022-06-07 00:36:31 -0300

    adding modifiers

--- FundingProject.sol
+++ FundingProject.sol
@@ -3,28 +3,46 @@ pragma solidity ^0.8.0;
 
 contract fundingProject {
   string public id;
-  string public name; 
+  string public name;
   string public description;
   address payable public author;
   uint public fundingGoal;
   uint public fundingRaised;
-  string public status = "Opened";
+  string public status = "Open";
 
-  constructor(string memory _id, string memory _name, string memory _description, uint _fundingGoal){
+
+  constructor(string memory _id, string memory _name, string memory _description, uint _fundingGoal)  { 
     id = _id;
     name = _name;
     description = _description;
-    fundingGoal = _fundingGoal;
     author = payable(msg.sender);
+    fundingGoal = _fundingGoal;
   }
 
-  function fundProject() payable public {
+  modifier onlyOwner() {
+    require(
+      msg.sender == author,
+      "Only owner can do this"
+    );
+    _;
+  }
+
+  modifier ownerNotdo(){
+    require(
+      msg.sender != author,
+      "The author can't do this"
+    );
+    _;
+  }
+
+  function fundProject() payable public ownerNotdo {
     author.transfer(msg.value);
     fundingRaised += msg.value;
   }
 
-  function changeStatus(string calldata newStatus) public {
+  function changeStatus(string calldata newStatus) public onlyOwner {
     status = newStatus;
   }
 
 }
+
