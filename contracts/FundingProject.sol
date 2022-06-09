4fb5dfc988f5a365f27a62263ecbf1510b14bc64
Nicolas Posa <nicolasposa@Nicolass-MacBook-Air.local>
2022-06-09 18:56:01 -0300

    add mapping and arrays for add more project

--- contracts/FundingProject.sol
+++ contracts/FundingProject.sol
@@ -1,76 +1,131 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract fundintProject {
-  enum FundraisingState {Opened, Closed}
-  struct Project {
-    string id;
-    string name;
-    string description;
-    uint fundingGoal;
-    address payable author;
-    string stateIs;
-    FundraisingState state;
-    uint fundingRaised;
-  }
-
-  Project public project;
-
-
-
-  constructor(string memory _id, string memory _name, string memory _description, uint _fundingGoal){
-    project = Project(_id, _name, _description, _fundingGoal, payable(msg.sender), "Open", FundraisingState.Opened, 0 );
-  }
-
-  event ChangeState(
-    string id,
-    address changer,
-    FundraisingState state,
-    string stateIs
-  );
-
-  event FundProject (
-    string id,
-    address sender,
-    uint amount
-  );
-
-  modifier isNotAuthor {
-    require(
-      project.author != msg.sender,
-      "The author can't fund the project"
-    );
-    _;
-  }
-
-  modifier isAuthor {
-    require(
-      project.author == msg.sender,
-      "Only author can change the state"
-    );
-    _;
-  }
-
-  function fundProject() payable public isNotAuthor {
-    require(project.state != FundraisingState.Closed, "The projct is Closed");
-    require(msg.value > 0, "Fund value must be greater than 0");
-    project.author.transfer(msg.value);
-    project.fundingRaised += msg.value;
-    emit FundProject(project.id, msg.sender, msg.value);
-  }
-
-  function changeState(FundraisingState newState) public isAuthor {
-    require(newState == FundraisingState.Opened || newState == FundraisingState.Closed, "The state must be 0 or 1");
-    require(newState != project.state, "The project is already in this state");
-    if(newState == FundraisingState.Opened) {
-      project.state = newState;
-      project.stateIs = "Open";
-    emit ChangeState(project.id, msg.sender, newState, project.stateIs);
-    }else if(newState == FundraisingState.Closed) {
-      project.state = newState;
-      project.stateIs = "Closed";
-      emit ChangeState(project.id, msg.sender, newState, project.stateIs);
+contract fundingProject {
+    enum FundraisingState {
+        Opened,
+        Closed
     }
-  }
 
+    struct Contribution {
+        address contributor;
+        uint256 amount;
+    }
+
+    struct Project {
+        string id;
+        string name;
+        string description;
+        uint256 fundingGoal;
+        address payable author;
+        FundraisingState state;
+        string stateIs;
+        uint256 fundingRaised;
+    }
+
+    Project[] public projects;
+    mapping(string => Contribution[]) public contributions;
+
+    event CreateProject(
+        string id,
+        string name,
+        string description,
+        uint256 fundingGoal
+    );
+
+    event FundProject(string id, address sender, uint256 amount);
+
+    event ChangeState(
+        string id,
+        address changer,
+        FundraisingState state,
+        string stateIs
+    );
+
+    modifier isAuthor(uint256 projectIndex) {
+        require(
+            projects[projectIndex].author == msg.sender,
+            "Only the author can change the state"
+        );
+        _;
+    }
+
+    modifier isNotAuthor(uint256 projectIndex) {
+        require(
+            projects[projectIndex].author != msg.sender,
+            "As author you can't fund your own project"
+        );
+        _;
+    }
+
+    function createProject(
+        string calldata id,
+        string calldata name,
+        string calldata description,
+        uint256 fundingGoal
+    ) public {
+        require(fundingGoal > 0, "The Goal must be greater than 0");
+        Project memory project = Project(
+            id,
+            name,
+            description,
+            fundingGoal,
+            payable(msg.sender),
+            FundraisingState.Opened,
+            "Open",
+            0
+        );
+        projects.push(project);
+        emit CreateProject(
+            project.id,
+            project.name,
+            project.description,
+            project.fundingGoal
+        );
+    }
+
+    function fundProject(uint256 projectIndex)
+        public
+        payable
+        isNotAuthor(projectIndex)
+    {
+        Project memory project = projects[projectIndex];
+        require(msg.value > 0, "Fund value must be greater than 0");
+        require(
+            project.state == FundraisingState.Opened,
+            "The project is closed"
+        );
+
+        project.author.transfer(msg.value);
+        project.fundingGoal += msg.value;
+        projects[projectIndex] = project;
+        emit FundProject(project.id, msg.sender, msg.value);
+    }
+
+    function changeState(FundraisingState newState, uint256 projectIndex)
+        public
+        isAuthor(projectIndex)
+    {
+        Project memory project = projects[projectIndex];
+        require(
+            newState != project.state,
+            "The state is defined with the same state"
+        );
+
+        if (newState == FundraisingState.Opened) {
+            project.state = newState;
+            project.stateIs = "Open";
+            projects[projectIndex] = project;
+            emit ChangeState(
+                project.id,
+                msg.sender,
+                project.state,
+                project.stateIs
+            );
+        } else if (newState == FundraisingState.Closed) {
+            project.state = newState;
+            project.stateIs = "Closed";
+            projects[projectIndex] = project;
+        }
+    }
 }
