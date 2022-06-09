cd772e3161c13ff54a79def1939c9e79a87ab900
Nicolas Posa <nicolasposa@Nicolass-MacBook-Air.local>
2022-06-09 15:11:24 -0300

    add enum for state

--- contracts/FundingProject.sol
+++ contracts/FundingProject.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract fundintProject {
+  enum FundraisingState {Opened, Closed}
   struct Project {
     string id;
     string name;
@@ -9,20 +10,22 @@ contract fundintProject {
     uint fundingGoal;
     address payable author;
     string stateIs;
-    uint state;
+    FundraisingState state;
     uint fundingRaised;
   }
 
   Project public project;
 
+
+
   constructor(string memory _id, string memory _name, string memory _description, uint _fundingGoal){
-    project = Project(_id, _name, _description, _fundingGoal, payable(msg.sender), "Open", 0, 0 );
+    project = Project(_id, _name, _description, _fundingGoal, payable(msg.sender), "Open", FundraisingState.Opened, 0 );
   }
 
   event ChangeState(
     string id,
     address changer,
-    uint state,
+    FundraisingState state,
     string stateIs
   );
 
@@ -34,7 +37,7 @@ contract fundintProject {
 
   modifier isNotAuthor {
     require(
-      project.author == msg.sender,
+      project.author != msg.sender,
       "The author can't fund the project"
     );
     _;
@@ -42,26 +45,28 @@ contract fundintProject {
 
   modifier isAuthor {
     require(
-      project.author != msg.sender,
+      project.author == msg.sender,
       "Only author can change the state"
     );
     _;
   }
 
   function fundProject() payable public isNotAuthor {
+    require(project.state != FundraisingState.Closed, "The projct is Closed");
+    require(msg.value > 0, "Fund value must be greater than 0");
     project.author.transfer(msg.value);
     project.fundingRaised += msg.value;
     emit FundProject(project.id, msg.sender, msg.value);
   }
 
-  function changeState(uint newState) public isAuthor {
-    require(newState == 0 || newState == 1, "The state must be 0 or 1");
+  function changeState(FundraisingState newState) public isAuthor {
+    require(newState == FundraisingState.Opened || newState == FundraisingState.Closed, "The state must be 0 or 1");
     require(newState != project.state, "The project is already in this state");
-    if(newState == 0) {
+    if(newState == FundraisingState.Opened) {
       project.state = newState;
       project.stateIs = "Open";
     emit ChangeState(project.id, msg.sender, newState, project.stateIs);
-    }else if(newState == 1) {
+    }else if(newState == FundraisingState.Closed) {
       project.state = newState;
       project.stateIs = "Closed";
       emit ChangeState(project.id, msg.sender, newState, project.stateIs);
